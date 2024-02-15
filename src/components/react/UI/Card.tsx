11c14e6254fcfc869f3838805d1ddab528b8d9af
ajn404 <nhy1423238345@163.com>
2024-02-15 11:13:00 +0800

    read and write

--- src/components/react/UI/Card.tsx
+++ src/components/react/UI/Card.tsx
@@ -1,6 +1,8 @@
 import { slugifyStr } from "@utils/slugify";
 import Datetime from "./Datetime";
 import type { CollectionEntry } from "astro:content";
+import Text from "@components/react/UI/Text";
+import { useEffect, useRef, useState } from "react";
 
 export interface Props {
   href?: string;
@@ -9,27 +11,83 @@ export interface Props {
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, modDatetime, description } = frontmatter;
+  const { title, pubDatetime, modDatetime, description, upDateTime } =
+    frontmatter;
+
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const obs = new IntersectionObserver(entries => {
+      if (!entries || entries.length === 0) return;
+      setIsVisible(entries[0].isIntersecting);
+    });
+    obs.observe(ref.current!);
+  }, []);
+  let image;
+
+  //564*846
+  let src = `/notes_astro3/assets/bg/${Math.floor(Math.random() * 6) + 1}.${
+    Math.floor(Math.random() * 10) % 2 === 1 ? "jpg" : "png"
+  }`;
+
+  let [backgroundImageStyle, setStyle] = useState({
+    backgroundImage: `radial-gradient(ellipse at top, rgb(${Math.floor(
+      Math.random() * 255
+    )},${Math.floor(Math.random() * 355) - 100},${
+      Math.floor(Math.random() * 255) % 250
+    }), transparent),
+            radial-gradient(ellipse at bottom, #4d9f0c, transparent)`,
+  });
+
+  useEffect(() => {
+    image = new Image();
+    image.src = src;
+    image.onload = () => {
+      if (isVisible)
+        setStyle({
+          backgroundImage: `url(${image.src})`,
+        });
+    };
+  }, [isVisible]);
+
+  useEffect(() => {
+    let element = ref.current!;
+
+    element.addEventListener("touchstart", function () {
+      this.classList.add("hover-effect");
+    });
+
+    element.addEventListener("touchend", function () {
+      this.classList.remove("hover-effect");
+    });
+  });
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
-    className: "text-lg font-medium decoration-dashed hover:underline",
+    className: "text-lg card__heading pt-4 font-medium ",
   };
-
   return (
-    <li className="my-6">
-      <a
-        href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
-      >
+    <a
+      className="card"
+      data-astro-prefetch
+      href={href}
+      ref={ref}
+      data-astro-reload
+    >
+      <div className="card__background" style={backgroundImageStyle}></div>
+      <div className="card__content pt-4">
         {secHeading ? (
           <h2 {...headerProps}>{title}</h2>
         ) : (
           <h3 {...headerProps}>{title}</h3>
         )}
-      </a>
-      <Datetime className="pt-4" datetime={pubDatetime} />
-      <p>{description}</p>
-    </li>
+
+        <Datetime className="pt-4" datetime={pubDatetime} />
+        <Text>
+          <h3>{description}</h3>
+        </Text>
+      </div>
+    </a>
   );
 }
