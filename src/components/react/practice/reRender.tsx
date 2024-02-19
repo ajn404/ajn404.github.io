b61a882f164d757a90fc18cb45c0e3c03d3d1327
ajn404 <nhy1423238345@163.com>
2024-02-19 15:11:05 +0800

    replace notes_astro

---
+++ src/components/react/practice/reRender.tsx
@@ -0,0 +1,37 @@
+// This is a React Quiz from BFE.dev
+
+import React, { useState, useEffect } from "react";
+
+function A(props: { state: number }) {
+  console.log("A");
+  return <B />;
+}
+
+function B() {
+  console.log("B");
+  return <C />;
+}
+
+function C() {
+  console.log("C");
+  return null;
+}
+
+function D() {
+  console.log("D");
+  return null;
+}
+
+export default function App() {
+  const [state, setState] = useState(0);
+  useEffect(() => {
+    setState(state => state + 1);
+  }, []);
+  console.log("App");
+  return (
+    <div>
+      <A state={state} />
+      <D />
+    </div>
+  );
+}
