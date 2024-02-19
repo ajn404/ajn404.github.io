5c467a4d6fa4b4d3e119b1d35ccff97f2bf7972d
ajn404 <nhy1423238345@163.com>
2024-02-19 16:25:13 +0800

    shadcn demo

---
+++ src/components/react/shadcn/demo/NavigationMenu.tsx
@@ -0,0 +1,27 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@shadcn/ui/navigation-menu";
+
+export default () => {
+  return (
+    <>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </>
+  );
+};
