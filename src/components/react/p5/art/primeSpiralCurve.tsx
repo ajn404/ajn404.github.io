b37bdf3b59c31ae5caf14fe664387d07334fb6e8
ajn404 <nhy1423238345@163.com>
2024-03-05 12:46:30 +0800

    prime spiral

--- src/components/react/p5/art/primeSpiralCurve.tsx
+++ src/components/react/p5/art/primeSpiralCurve.tsx
@@ -7,7 +7,9 @@ import { cn } from "@utils/utils";
 
 export default () => {
   let [Num, setNum] = useState(5);
+  let [frameRate, setFrameRate] = useState(60);
   let debounceNum = useDebounce(Num, 200);
+  let debounceFrameRate = useDebounce(frameRate, 200);
 
   const sketch = useCallback(
     (p: p5) => {
@@ -15,42 +17,53 @@ export default () => {
 
       const setup = () => {
         p.createCanvas(p.windowWidth / 2, p.windowWidth / 2);
-        for (let i = 0; i < (p.windowWidth * debounceNum) / p.PI; i++) {
+        let len = (p.windowWidth * debounceNum) / (p.PI * 8);
+        let de = len % 4;
+        for (let i = 0; i < len - de; i++) {
           let vector = p.createVector(i, i);
           points.push(vector);
         }
-        p.background(255);
+        p.setAttributes("antialias", true);
+        // p.background(255);
+        p.frameRate(debounceFrameRate);
+      };
+      let i = 0;
+      const draw = () => {
         p.translate(p.windowWidth / 4, p.windowWidth / 4);
-        p.stroke(0);
-        p.fill(0);
-        p.line(0, p.height / 2, p.width, p.height / 2);
-        p.line(p.width / 2, 0, p.width / 2, p.height);
-        p.noStroke();
-        for (let i = 0; i < points.length; i++) {
+        p.strokeWeight(debounceNum);
+        p.noFill();
+        if (i < points.length) {
           let len = points[i].x;
-          let x1 = p.cos(len) * len;
-          let y1 = p.sin(len) * len;
-          x1 = Number(x1.toFixed(2));
-          y1 = Number(y1.toFixed(2));
-          p.ellipse(
-            x1 / debounceNum,
-            y1 / debounceNum,
-            debounceNum,
-            debounceNum
-          );
+          let len2 = points[i + 1].x;
+          let len3 = points[i + 2].x;
+          let len4 = points[i + 3].x;
+          let [x1, y1] = [p.cos(len) * len, p.sin(len) * len];
+          let [x2, y2] = [p.cos(len2) * len2, p.sin(len2) * len];
+          let [x3, y3] = [p.cos(len3) * len3, p.sin(len3) * len3];
+          let [x4, y4] = [p.cos(len4) * len4, p.sin(len4) * len4];
+          [x1, x2, x3, x4, y1, y2, y3, y4].forEach(item => {
+            item = Number(item.toFixed(2));
+          });
+          p.curve(...[x1, y1, x2, y2, x3, y3, x4, y4]);
+          i += 4;
+        } else {
+          p.erase();
+          p.fill(0);
+          p.circle(0, 0, p.width * 2);
+          p.noErase();
+          i = 0;
         }
       };
-      const draw = () => {};
       const resize = () => p.setup();
       p.setup = setup;
       p.draw = draw;
       p.windowResized = resize;
     },
-    [debounceNum]
+    [debounceNum, debounceFrameRate]
   );
   return (
     <>
-      数量{Num}
+      半径{Num},半径越小数量越多
       <Slider
         defaultValue={[5]}
         max={10}
@@ -61,6 +74,18 @@ export default () => {
           setNum(value[0]);
         }}
         value={[Num]}
+      />
+      帧率{frameRate}
+      <Slider
+        defaultValue={[60]}
+        max={120}
+        min={1}
+        step={1}
+        className={cn("w-[100%] m-4")}
+        onValueChange={value => {
+          setFrameRate(value[0]);
+        }}
+        value={[frameRate]}
       />
       <Basic sketch={sketch}></Basic>
     </>
--- src/components/react/p5/art/primeSpiralStep.tsx
+++ src/components/react/p5/art/primeSpiralStep.tsx
@@ -7,7 +7,9 @@ import { cn } from "@utils/utils";
 
 export default () => {
   let [Num, setNum] = useState(5);
+  let [frameRate, setFrameRate] = useState(60);
   let debounceNum = useDebounce(Num, 200);
+  let debounceFrameRate = useDebounce(frameRate, 200);
 
   const sketch = useCallback(
     (p: p5) => {
@@ -19,44 +21,36 @@ export default () => {
           let vector = p.createVector(i, i);
           points.push(vector);
         }
+        p.setAttributes("antialias", true);
         p.background(255);
+        p.frameRate(debounceFrameRate);
+      };
+      let i = 0;
+      const draw = () => {
         p.translate(p.windowWidth / 4, p.windowWidth / 4);
-        p.stroke(0);
-        p.fill(0);
-        p.line(0, p.height / 2, p.width, p.height / 2);
-        p.line(p.width / 2, 0, p.width / 2, p.height);
-        p.noStroke();
-        for (let i = 0; i < points.length; i++) {
+        p.strokeWeight(debounceNum);
+        if (i < points.length) {
           let len = points[i].x;
           let x1 = p.cos(len) * len;
           let y1 = p.sin(len) * len;
           x1 = Number(x1.toFixed(2));
           y1 = Number(y1.toFixed(2));
-          p.ellipse(
-            x1 / debounceNum,
-            y1 / debounceNum,
-            debounceNum,
-            debounceNum
-          );
+          p.point(x1, y1);
+          i++;
+        } else {
+          i = 0;
         }
-      };
-      const draw = () => {
-        p.stroke(0);
-        p.strokeWeight(debounceNum / 4);
-        p.fill(0);
-        p.line(0, p.height / 2, p.width, p.height / 2);
-        p.line(p.width / 2, 0, p.width / 2, p.height);
       };
       const resize = () => p.setup();
       p.setup = setup;
       p.draw = draw;
       p.windowResized = resize;
     },
-    [debounceNum]
+    [debounceNum, debounceFrameRate]
   );
   return (
     <>
-      数量{Num}
+      半径{Num},半径越小数量越多
       <Slider
         defaultValue={[5]}
         max={10}
@@ -67,6 +61,18 @@ export default () => {
           setNum(value[0]);
         }}
         value={[Num]}
+      />
+      帧率{frameRate}
+      <Slider
+        defaultValue={[60]}
+        max={120}
+        min={1}
+        step={1}
+        className={cn("w-[100%] m-4")}
+        onValueChange={value => {
+          setFrameRate(value[0]);
+        }}
+        value={[frameRate]}
       />
       <Basic sketch={sketch}></Basic>
     </>
