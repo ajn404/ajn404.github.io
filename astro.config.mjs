c9a40e3eff5620491431ca657c8802013b822386
ajn404 <nhy1423238345@163.com>
2024-07-16 13:34:28 +0800

    useEffect import

--- astro.config.mjs
+++ astro.config.mjs
@@ -105,6 +105,7 @@ export default defineConfig({
         external: [
           "wavesurfer.js",
           "wavesurfer.js/dist/plugins/spectrogram.esm.js",
+          "cesium"
         ],
       },
     },
