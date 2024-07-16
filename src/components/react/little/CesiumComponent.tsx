ab48e9fe80ecf2d1a7fbc0c15fd92e6e5ffb7286
ajn404 <nhy1423238345@163.com>
2024-07-16 13:27:25 +0800

    window not defined

--- src/components/react/little/CesiumComponent.tsx
+++ src/components/react/little/CesiumComponent.tsx
@@ -1,25 +1,31 @@
 import React, { useEffect } from "react";
-import { Viewer, Entity, EntityDescription } from "resium";
+import { Viewer, Entity, EntityDescription, PointGraphics } from "resium";
 import { Cartesian3 } from "cesium";
 
 const CesiumComponent: React.FC = () => {
   useEffect(() => {
     import("cesium/Source/Widgets/widgets.css");
   }, []);
-
+  //东经109°4′48″～109°58′42″、北纬29°50′33″～30°39′30″
+  const position = Cartesian3.fromDegrees(109.077, 29.833, 100);
+  const pointGraphics = { pixelSize: 10 };
   return (
     <>
-      <Viewer full infoBox={false}>
-        {/* <Entity
-                name="Tokyo"
-                position={Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
-                point={{ pixelSize: 10 }}
-            >
-                <EntityDescription>
-                    <h1>Tokyo</h1>
-                    <p>Tokyo is the capital of Japan.</p>
-                </EntityDescription>
-            </Entity> */}
+      <Viewer
+        infoBox={false}
+        style={{
+          position: "relative",
+          zIndex: 99999,
+        }}
+      >
+        <Entity
+          description="Hello, world."
+          name="恩施"
+          position={position}
+          point={pointGraphics}
+        >
+          <PointGraphics pixelSize={10} />
+        </Entity>
       </Viewer>
     </>
   );
