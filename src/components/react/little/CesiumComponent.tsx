9dce013a35db1d12a749f3289e9fc85cd1d363e5
ajn404 <nhy1423238345@163.com>
2024-07-16 15:28:37 +0800

    try not use resium

--- src/components/react/little/CesiumComponent.tsx
+++ src/components/react/little/CesiumComponent.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect } from "react";
-import { Cartesian3 } from "cesium";
-import { Viewer, Entity, PointGraphics } from "resium";
+// import { Cartesian3 } from "cesium";
+// import { Viewer, Entity, PointGraphics } from "resium";
+
+import * as Cesium from "cesium";
 
 const CesiumComponent: React.FC = () => {
   useEffect(() => {
     import("cesium/Source/Widgets/widgets.css");
+    const viewer = new Cesium.Viewer("cesiumContainer");
   }, []);
   //东经109°4′48″～109°58′42″、北纬29°50′33″～30°39′30″
-  const position = Cartesian3.fromDegrees(109.077, 29.833, 100);
-  const pointGraphics = { pixelSize: 10 };
+  //   const position = Cartesian3.fromDegrees(109.077, 29.833, 100);
+  //   const pointGraphics = { pixelSize: 10 };
   return (
     <>
-      <Viewer
+      {/* <Viewer
         infoBox={false}
         style={{
           position: "relative",
@@ -26,7 +29,8 @@ const CesiumComponent: React.FC = () => {
         >
           <PointGraphics pixelSize={10} />
         </Entity>
-      </Viewer>
+      </Viewer> */}
+      <div id="cesiumContainer"></div>
     </>
   );
 };
