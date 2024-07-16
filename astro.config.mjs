820c3b4b2f7f7fbc1e33ceb858fcad8f312c8d6a
ajn404 <nhy1423238345@163.com>
2024-07-16 16:05:13 +0800

    try try

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,8 +14,15 @@ import css from "rollup-plugin-css-only";
 import { fileURLToPath } from "url";
 import { remarkReadingTime } from "./plugin/remark-reading-time.mjs";
 import cesium from "vite-plugin-cesium";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 const __filenameNew = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filenameNew);
+
+const cesiumSource = "node_modules/cesium/Build/Cesium";
+// This is the base url for static files that CesiumJS needs to load.
+// Set to an empty string to place the files at the site's root path
+const cesiumBaseUrl = "cesiumStatic";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
@@ -83,13 +90,31 @@ export default defineConfig({
     extendDefaultPlugins: true,
   },
   vite: {
-    plugins: [cesium()],
+    // plugins: [cesium()],
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
     },
     ssr: {
       noExternal: ["@notes/editor", "@shoelace-style/shoelace"],
-      },
+    },
+    define: {
+      // Define relative base path in cesium for loading assets
+      // https://vitejs.dev/config/shared-options.html#define
+      CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
+    },
+    plugins: [
+      // Copy Cesium Assets, Widgets, and Workers to a static directory.
+      // If you need to add your own static files to your project, use the `public` directory
+      // and other options listed here: https://vitejs.dev/guide/assets.html#the-public-directory
+      viteStaticCopy({
+        targets: [
+          { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
+          { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
+        ],
+      }),
+    ],
     build: {
       rollupOptions: {
         plugins: [
--- src/components/react/little/CesiumComponent.tsx
+++ src/components/react/little/CesiumComponent.tsx
@@ -2,13 +2,32 @@ import React, { useEffect } from "react";
 // import { Cartesian3 } from "cesium";
 // import { Viewer, Entity, PointGraphics } from "resium";
 
-import { Viewer } from "cesium";
+import {
+  Cartesian3,
+  Math as CesiumMath,
+  Terrain,
+  Viewer,
+  createOsmBuildingsAsync,
+} from "cesium";
+
+import "cesium/Build/Cesium/Widgets/widgets.css";
 
 const CesiumComponent: React.FC = () => {
   useEffect(() => {
-    import("cesium/Source/Widgets/widgets.css");
-    new Viewer("cesiumContainer", {
-      infoBox: true,
+    // import("cesium/Source/Widgets/widgets.css");
+    const viewer = new Viewer("cesiumContainer", {
+      infoBox: false,
+      terrain: Terrain.fromWorldTerrain(),
+    });
+    viewer.camera.flyTo({
+      destination: Cartesian3.fromDegrees(109.177, 29.833, 4100),
+      orientation: {
+        heading: CesiumMath.toRadians(0.0),
+        pitch: CesiumMath.toRadians(-15.0),
+      },
+    });
+    createOsmBuildingsAsync().then(buildingTileset => {
+      viewer.scene.primitives.add(buildingTileset);
     });
   }, []);
   //东经109°4′48″～109°58′42″、北纬29°50′33″～30°39′30″
