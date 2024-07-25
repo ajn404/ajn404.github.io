955a46c70dbd9d52a72844914750228df16ce5d7
ajn404 <nhy1423238345@163.com>
2024-07-25 17:36:59 +0800

    垃圾prettier

--- src/components/react/observablehq/ajn404/generateSvg.tsx
+++ src/components/react/observablehq/ajn404/generateSvg.tsx
@@ -6,10 +6,9 @@ function Bnbshader() {
   const open = useRef(false);
   useEffect(() => {
     const runtime = new Runtime();
-    //@ts-ignore
-    import(
-      "https://api.observablehq.com/@ajn404-ws/generating-svgs@416.js?v=4&api_key=21ccdca7785e2208d1de7207e69c43635468f5b8"
-    ).then(module => {
+    const url =
+      "https://api.observablehq.com/@ajn404-ws/generating-svgs@416.js?v=4&api_key=21ccdca7785e2208d1de7207e69c43635468f5b8";
+    import(url).then(module => {
       const notebook = module.default;
       runtime.module(notebook, Inspector.into(ref.current));
     });
