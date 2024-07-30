95e3000dd3eaa17f40ef212940cfe5400e4c6bc9
ajn404 <nhy1423238345@163.com>
2024-07-30 14:49:11 +0800

    try to build android

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,16 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
