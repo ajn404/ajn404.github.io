5ca7da90a872849e169ed65bf32d3dfb6ac979cd
ajn404 <nhy1423238345@163.com>
2024-07-31 09:13:45 +0800

    update config

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-fn main() {
+pub fn run(){
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
