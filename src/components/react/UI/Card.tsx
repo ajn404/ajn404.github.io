344054e6ad447e20ba26cbb4c16e2efa0469ee6f
ajn404 <nhy1423238345@163.com>
2024-09-04 11:58:00 +0800

    回归朴素

--- src/components/react/UI/Card.tsx
+++ src/components/react/UI/Card.tsx
@@ -2,8 +2,7 @@ import { slugifyStr } from "@utils/slugify";
 import Datetime from "./Datetime";
 import type { CollectionEntry } from "astro:content";
 import Text from "@components/react/UI/Text";
-import { useEffect, useRef, useState, useMemo, useCallback } from "react";
-import classNames from "classnames";
+import { useRef } from "react";
 
 export interface Props {
   href?: string;
@@ -11,95 +10,15 @@ export interface Props {
   secHeading?: boolean;
   showBackground?: boolean;
 }
-export default function Card({
-  href,
-  frontmatter,
-  secHeading = true,
-  showBackground = true,
-}: Props) {
+export default function Card({ href, frontmatter, secHeading = true }: Props) {
   const { title, pubDatetime, description } = frontmatter;
-  const [isVisible, setIsVisible] = useState(false);
-  const [loading, setLoading] = useState(true); // 新增加载状态
   const ref = useRef<HTMLAnchorElement>(null);
-  const src = useMemo(() => {
-    return `/assets/bg/${Math.floor(Math.random() * 12) + 1}.${Math.random() < 0.5 ? "jpg" : "png"}`;
-  }, []);
-  const [backgroundImageStyle, setBackgroundImageStyle] = useState({
-    backgroundImage: "",
-  });
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
     className: "text-lg card__heading pt-4 font-medium",
   };
-  useEffect(() => {
-    const loadStyles = async () => {
-      if (showBackground) {
-        await import("@styles/card.scss");
-      } else {
-        await import("@styles/searchCard.scss");
-      }
-    };
-    loadStyles();
-  }, [showBackground]);
-  useEffect(() => {
-    const obs = new IntersectionObserver(entries => {
-      if (entries.length > 0) {
-        setIsVisible(entries[0].isIntersecting);
-      }
-    });
-    if (ref.current) {
-      obs.observe(ref.current);
-    }
-    return () => {
-      if (ref.current) {
-        obs.unobserve(ref.current);
-      }
-    };
-  }, []);
-  useEffect(() => {
-    const image = new Image();
-    image.src = src;
-    image.onload = () => {
-      setLoading(false); // 图片加载完成，设置加载状态为 false
-      if (isVisible) {
-        setBackgroundImageStyle({ backgroundImage: `url(${image.src})` });
-      }
-    };
-  }, [isVisible, src]);
-  const handleTouchStart = useCallback(() => {
-    ref.current?.classList.add("hover-effect");
-  }, []);
-  const handleTouchEnd = useCallback(() => {
-    ref.current?.classList.remove("hover-effect");
-  }, []);
-  let randomNum = Math.floor(Math.random() * 10) + 1;
-
-  useEffect(() => {
-    const element = ref.current!;
-    element.addEventListener("touchstart", handleTouchStart, { passive: true });
-    element.addEventListener("touchend", handleTouchEnd);
-    return () => {
-      element.removeEventListener("touchstart", handleTouchStart);
-      element.removeEventListener("touchend", handleTouchEnd);
-    };
-  }, [handleTouchStart, handleTouchEnd]);
   return (
-    <a
-      className={classNames("card", { "hover-effect": isVisible })}
-      href={href}
-      ref={ref}
-      data-astro-prefetch
-    >
-      <div className="card__background absolute" style={backgroundImageStyle}>
-        {loading && (
-          <div
-            className={`w-10 absolute right-0 top-0 h-10 rounded-full random-gradient${randomNum}`}
-            style={{
-              animation: `loadingcard ${randomNum / 2}s 0.1s linear infinite`,
-            }}
-          ></div>
-        )}{" "}
-      </div>
+    <a href={href} ref={ref} data-astro-prefetch>
       <div className="card__content pt-4">
         {secHeading ? (
           <h2 {...headerProps}>{title}</h2>
