16a9be7b9503eb781664e2cce62c8a54808f4208
ajn404 <nhy1423238345@163.com>
2024-12-06 15:20:08 +0800

    feat: commit

---
+++ src/components/react/aceternity/demo/compare.tsx
@@ -0,0 +1,16 @@
+import { Compare } from "../ui/compare";
+
+export default () => {
+  return (
+    <div className="p-4 border rounded-3xl dark:bg-neutral-900 bg-neutral-100  border-neutral-200 dark:border-neutral-800 px-4">
+      <Compare
+        firstImage="/assets/svg/ru01.svg"
+        secondImage="/assets/svg/ru02.svg"
+        firstImageClassName="object-cover object-center"
+        secondImageClassname="object-cover object-center"
+        className=" w-full "
+        slideMode="drag"
+      />
+    </div>
+  );
+};
