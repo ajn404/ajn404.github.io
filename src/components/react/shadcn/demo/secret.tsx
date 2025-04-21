43454b2dc43fb18402769e20da8ac19b924ac1db
ajn404 <nhy1423238345@163.com>
2025-04-21 16:23:59 +0800

    Refactor code structure for improved readability and maintainability

--- src/components/react/shadcn/demo/secret.tsx
+++ src/components/react/shadcn/demo/secret.tsx
@@ -7,9 +7,9 @@ import ComponentName from "@components/react/aceternity/demo/compare.tsx";
 
 interface SecretContentProps {
   password: string;
+  videoSrc: string;
 }
-
-export default ({ password }: SecretContentProps) => {
+export default ({ password, videoSrc }: SecretContentProps) => {
   const [inputPassword, setInputPassword] = useState("");
   const [isUnlocked, setIsUnlocked] = useState(false);
   const [error, setError] = useState(false);
@@ -30,7 +30,16 @@ export default ({ password }: SecretContentProps) => {
           <Unlock className="h-4 w-4" />
           <span className="text-sm">内容已解锁</span>
         </div>
-        <ComponentName />
+        {!videoSrc && <ComponentName />}
+        {videoSrc && (
+          <video
+            className="h-[50vh] w-auto m-auto rounded-lg shadow-lg"
+            controls
+            src={videoSrc}
+            autoPlay
+            loop
+          />
+        )}
       </div>
     );
   }
