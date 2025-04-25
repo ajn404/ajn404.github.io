5fb7d6fe864fd9a2c739fb42ed82ddd741aff120
ajn404 <nhy1423238345@163.com>
2025-04-25 16:15:37 +0800

    优化 TimelineDemo 组件的布局，移除多余的宽度限制；更新首页的主要内容和标题，增强可读性

--- src/components/react/UI/timeline.tsx
+++ src/components/react/UI/timeline.tsx
@@ -105,7 +105,7 @@ export default function TimelineDemo() {
     },
   ];
   return (
-    <div className="w-full pb-8 md:w-1/2">
+    <div className="w-full pb-8">
       <Timeline data={data} />
     </div>
   );
